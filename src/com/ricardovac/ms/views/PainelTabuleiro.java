879bb19bf7c049376ab58a55b8aaf9aa1ce7e044
ricardovac <ricardoviicius@gmail.com>
2023-03-18 12:34:33 -0300

    feat(views): tela principal e painel tabuleiro

---
+++ src/com/ricardovac/ms/views/PainelTabuleiro.java
@@ -0,0 +1,11 @@
+package com.ricardovac.ms.views;
+
+import com.ricardovac.ms.models.Tabuleiro;
+
+import javax.swing.*;
+
+public class PainelTabuleiro extends JPanel {
+    public PainelTabuleiro(Tabuleiro tabuleiro) {
+
+    }
+}
